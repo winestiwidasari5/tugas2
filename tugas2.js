474cd91df4eab9e0fa96866b32513dccc16dd2dd
winestiwidasari5 <124061505+winestiwidasari5@users.noreply.github.com>
2023-10-04 19:52:51 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,9 @@
+let firstName = "Siti";
+let lastName = "Marni";
+let age = 22;
+let isMarried = false
+
+console.log(" Nama Depan : "+ firstName)
+console.log(" Nama Belakang : "+ lastName)
+console.log(" Usia : "+age)
+console.log(" Menikah : "+ isMarried)
